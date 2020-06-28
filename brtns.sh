124245b02b11662caac8bb63499eb239463bc826
LIFE <dkrshw65@gmail.com>
2020-06-28 12:07:41 -0400

    refactored

--- brtns.sh
+++ brtns.sh
@@ -11,12 +11,22 @@ then
   exit 0;
 fi
 
+# Convert value to decimal like 0.1,0.3...
+LEVEL=$1
+brightness_level="$(( $LEVEL / 100 )).$(( $LEVEL % 100 ))"
+
+# Limitation
+if(( $LEVEL <= 0 || $LEVEL > 100 ))
+then
+   echo "Brightness limit exceed, choose from 1 to 100"
+   exit 0;
+fi
 
 # Find the Monitor Name
 monitor=$(xrandr | grep " connected" | cut -f1 -d " ")
 
 # Now adjust Brightness
 
-xrandr --output $monitor --brightness $1;
+xrandr --output $monitor --brightness $brightness_level;
 
-echo "Adjusted"
+echo "Adjusted to $LEVEL"
